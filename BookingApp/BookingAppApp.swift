fc285c491d234d4d6fbd182eafeed74a60f68f29
jschwaneberg <js@iroin.io>
2023-12-18 16:40:36 +0100

    Initial Commit

---
+++ BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Julian on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
